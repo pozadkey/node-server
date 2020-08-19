e64a70c5264f967723adf35983ac48dc384be57e
pozadkey <pozartng@gmail.com>
2020-08-19 14:13:01 -0700

    updated the files

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
-const http = require("http"); 
-const fs = require("fs");
-const qs = require("querystring");
-const localhost = "127.0.0.1";
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
+const localhost = '127.0.0.1';
 const port = 3000;
 
 //create server
-const server = http.createServer(function(req, res){
-    //making a GET request to the specifed url
-    if (req.url === "/" && req.method === "GET"){
-        console.log(`a ${req.method} request was made at ${req.url}`);
-        res.writeHead(200, {"Content-Type":"text/html"});
-        fs.createReadStream(__dirname + "/index.html", "utf8").pipe(res);
-    } 
-    //making a POST request to the specifed url
-    else if (req.url === "/" && req.method === "POST"){
-        console.log(`a ${req.method} request was made at ${req.url}`);
-        var body = "";
-        req.on("data", function(chunk){
-            body += chunk;
-        });
-
-        req.on("end", ()=>{
-            //parse the POST request body to JSON
-            var data = qs.parse(body);
-            let name = JSON.stringify(data.name);
-            res.writeHead(200);
-            //display once the POST request has been parsed
-            res.end(`Hello ${name}! Welcome to WeJapa Internships.`);
-            console.log(data);    
-        });
+const server = http.createServer((req, res)=>{
+    let body = '';
+    //handle get request
+    if (req.method === 'GET' && req.url === '/'){
+        console.log(`request was made at ${req.url} using ${req.method}`);
+        res.writeHead(200, {'Content-Type':'text/html'})
+        fs.readFile('index.html', 'utf8', (err, data)=>{
+            if (err) throw err;
+            else
+            res.write(data);
+            res.end();
+        })
     }
-    //handling error for incorrect url
+    //handle post request
+    else if (req.method === 'POST' && req.url === '/'){
+        console.log(`request was made at ${req.url} using ${req.method}`);
+        req.on('data', (data)=>{
+            body += data;
+        })
+        req.on('end', ()=>{
+            let data = qs.parse(body),
+            //convert data to json format
+            info = JSON.stringify(data);
+            //convert json to object
+            mydata = JSON.parse(info);
+            let name = mydata.name;
+            res.writeHead(200, {'Content-Type':'text/html'})
+            res.end(`Hello ${name}! Welcome to WeJapa Internships.`)
+        })
+    }
+
     else {
-        res.writeHead(404);
-        res.end(`page not found.`);
+        res.writeHead(404, {'Content-Type':'text/plain'})
+        res.end(`Sorry! Page not found.`)
     }
-});
+})
 
-//run server and check for errors
+    
+
 server.listen(port, localhost, (err)=>{
-    if (err) {
-        console.log(`oops! something is wrong.`);
-    } else {
-        console.log(`server is running at ${localhost}:${port}`);
-    }
-});
+    if (err)
+    console.log(`oops! server failed to start ${err}`);
+    else 
+    console.log(`server is running at ${localhost}:${port}`);
+})
